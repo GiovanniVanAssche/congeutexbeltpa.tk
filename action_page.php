9ebd41c03c07c5e12dcb0bda94abc8d8f8af5d62
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-01 21:24:14 +0100

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -8,9 +8,12 @@ include('../library/Requests.php');
 // Next, make sure Requests can load internal classes
 Requests::register_autoloader();
 
-?>
- Welkom <?php echo $_POST[uname]; ?><br>
- Your email address is: <?php echo $_POST[psw]; ?>
+// Now let's make a request!
+$request = Requests::post('login.html', array('Accept' => 'application/json'));
+// Check what we received
+var_dump($request);
+  
+  ?>
 
  </body>
  </html> 
