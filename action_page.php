94c945c3e0f92e7a1bfa3fa7498187ddf4ce4f72
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-01 21:21:05 +0100

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
  <body>
-<?php
-    use /login.html;
-   ?>
+  <?php
+ 
+ // First, include Requests
+include('../library/Requests.php');
+// Next, make sure Requests can load internal classes
+Requests::register_autoloader();
 
+?>
  Welkom <?php echo $_POST[uname]; ?><br>
  Your email address is: <?php echo $_POST[psw]; ?>
 
