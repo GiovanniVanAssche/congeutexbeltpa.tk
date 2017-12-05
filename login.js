9bf465a86d138f9d958e14c329383aa591607899
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-05 21:28:42 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,37 @@
-
-function pasuser(form) {
-  if (form.id.value=="JavaScript") { 
-    if (form.pass.value=="Kit") {              
-      location="index.html" ;
-    } 
-    else {
-      alert("Invalid Password")
-    }
-  } 
-  else {  
-    alert("Invalid UserID")
-  }
+<script language="javascript">
+	
+   var loginUname = ["Giovanni Van Assche","Davy Vercruysse"];
+   var loginPsw = ["06044","01235"];
+   var LengthFor;
+   function check(form){
+	   
+   if (loginUname.length == loginPsw.length){
+   LengthFor = loginUname.length;
+   }
+   else{
+    alert("Please contact your administrator");
+   }
+	   
+	   for (var i = 0; i < LengthFor ; i++)
+ {
+	if ((form.userid.value == loginUname[i]) && (form.pswrd.value == loginPsw[i]))
+	{
+		valid = true;
+		break;
+	}
 }
+	if (valid)
+	{
+		window.location.assign('https://giovannivanassche.github.io/congeutexbeltpa.tk/index.html');
+		return false;
+	}
+	    else{
+	    alert("De opgegeven combinatie komt niet overeen");
+   }
+   
+   
+
+   }
+
+ 
+</script>
