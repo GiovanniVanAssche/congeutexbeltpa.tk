8c73ddf1f8b8a3a702dd801e8a911a29448dd65c
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-06 22:05:21 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,28 +1,26 @@
-var loginUname = ["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"];    
-var loginPsw = ["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"];    
-var LengthFor;
-var Name;
-var positionName;
-
-function check(form){         
-  if (loginUname.length == loginPsw.length){    
-    LengthFor = loginUname.length;  
-  }    
-  
-  else{     
-    alert("Please contact your administrator");    
-  }          
-  
-  for (var i = 0; i < LengthFor ; i++){  
-    if ((form.userid.value == loginUname[i]) && (form.pswrd.value == loginPsw[i])){   
-      valid = true;
-      break;  
-    }    
-  }   
-  
-  if (valid){    
-    window.location.assign('https://giovannivanassche.github.io/congeutexbeltpa.tk/index.html');    
-    return false;   
-    break;
-  } 
-}
+ var loginUname = ["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"];    
+ var loginPsw = ["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"];    
+ var LengthFor;
+ var Name;
+ 
+  function check(form){         
+    if (loginUname.length == loginPsw.length){    
+      LengthFor = loginUname.length;
+    }    
+    
+    else{     
+     alert("Please contact your administrator");    
+       }          
+   
+    for (var i = 0; i < LengthFor ; i++)  {  
+      if ((form.userid.value == loginUname[i]) && (form.pswrd.value == loginPsw[i]))  {   
+        valid = true;
+        break;  
+      }     
+   }   
+   
+   if (valid)   {    
+     window.location.assign('https://giovannivanassche.github.io/congeutexbeltpa.tk/index.html');    
+     return false;   
+   }           
+ }
