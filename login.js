c748fbc69d7a5ffb314041bdc76d1a7f6c8e4d08
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-11 15:28:11 +0100

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,11 @@ var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Mich
             InhaalRust :["10","4,5","2","0","0","0","0","0","0","0","0","0","0"],
             Uren : ["48,52","120,22","0","0","0","0","0","0","0","0","0","0","0"],
             Jaarlijks : ["20","20","20","20","20","20","20","20","20","20","20","20","20"]}; 
+globalVariable={example_Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"]}; 
 var Index;
+var Verlof_Uren;
+var Verlof_InhaalRust;
+var Verlof_Jaarlijks
 
 function check(form){  
           
@@ -21,3 +25,4 @@ function check(form){
      return false;   
    }           
  }
+
