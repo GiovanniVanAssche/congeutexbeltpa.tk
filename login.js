559827f44822ab5708cc7feebc53ec85bfd8795d
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-11 11:12:18 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
- var users = { Name:[Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"], psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"]};    
+var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"], 
+             Psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"]};    
 
- 
-  function check(form){            
+function check(form){            
    
     for (var i = 0; i < 12 ; i++)  {  
-      if ((form.userid.value == users.Name[i]) && (form.pswrd.value == users.psw[i]))  {   
+      if ((form.userid.value == users.Name[i]) && (form.pswrd.value == users.Psw[i]))  {   
         valid = true;
         break;  
       }     
