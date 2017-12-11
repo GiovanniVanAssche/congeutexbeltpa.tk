699abfb8bd319a0f20d479cee65a4c97cbc63ebb
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-11 19:19:21 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,18 @@
+var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"],             
+    Psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"],                
+    InhaalRust :["10","4,5","2","0","0","0","0","0","0","0","0","0","0"],            
+    Uren : ["48,52","120,22","0","0","0","0","0","0","0","0","0","0","0"],            
+    Jaarlijks : ["20","20","20","20","20","20","20","20","20","20","20","20","20"]}; 
+
+var Verlof_Uren;
+var Verlof_InhaalRust;
+var Verlof_Jaarlijks;
+
 function check(form){  
           
     for (var i = 0; i < users.Name.length ; i++)  {  
       if ((form.userid.value == users.Name[i]) && (form.pswrd.value == users.Psw[i]))  {   
-        Index = i;
+       sessionStorage.setItem("UserIndex",i);
         valid = true;
         break;  
       }     
