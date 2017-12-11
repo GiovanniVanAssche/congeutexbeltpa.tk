599af39ab73aebedd539fd565b670f963fc1c776
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-11 11:46:49 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,8 @@
 var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"], 
              Psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"]};    
+var Verlof_uren = "28.14";      
+var Verlof_IR = "20.5";  
+var Verlof_Jaarlijks = "14";
 
 function check(form){            
    
