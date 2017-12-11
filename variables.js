bb8c4e3c4dccb34863b793333944f3ec8606beaa
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-11 15:42:00 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"],             
+    Psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"],                
+    InhaalRust :["10","4,5","2","0","0","0","0","0","0","0","0","0","0"],            
+    Uren : ["48,52","120,22","0","0","0","0","0","0","0","0","0","0","0"],            
+    Jaarlijks : ["20","20","20","20","20","20","20","20","20","20","20","20","20"]}; 
+
+var Index;
+var Verlof_Uren;
+var Verlof_InhaalRust;
+var Verlof_Jaarlijks;
