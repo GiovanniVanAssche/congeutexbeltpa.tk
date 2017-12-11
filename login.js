96f0448171fb1e9961f562d3980d54f910cca50e
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-11 15:40:40 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,7 @@
-var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"], 
-            Psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"], 
-            InhaalRust :["10","4,5","2","0","0","0","0","0","0","0","0","0","0"],
-            Uren : ["48,52","120,22","0","0","0","0","0","0","0","0","0","0","0"],
-            Jaarlijks : ["20","20","20","20","20","20","20","20","20","20","20","20","20"]}; 
-globalVariable={example_Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"]}; 
-var Index;
-var Verlof_Uren;
-var Verlof_InhaalRust;
-var Verlof_Jaarlijks
-
 function check(form){  
           
     for (var i = 0; i < users.Name.length ; i++)  {  
       if ((form.userid.value == users.Name[i]) && (form.pswrd.value == users.Psw[i]))  {   
-        alert("Login OK");
         Index = i;
         valid = true;
         break;  
