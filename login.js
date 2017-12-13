c6953c46f1c192ca28bcf318927485dada227a8f
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-13 09:05:45 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 var users = { Name:["Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"],             
-    Psw:["06044","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235","01235"],
+    Psw:["06044","01235","01235","04549utb","01235","01235","01235","01235","01235","01235","01235","01235","01235"],
     Taalcode:["NL","FR","FR","NL","FR","NL","NL","FR","FR","FR","FR","NL","FR"]}; 
 
 
