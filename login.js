e3d0b19637835db317855f7859506e9ecea5d443
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2017-12-14 12:41:54 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
-var users = { Name:["Line Vancauwenberghe","Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq","Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse","Thierry Verza"],             
-    Psw:["05193utb","06044utb","04970utb","04439utb","04549utb","04922utb","01235","01235","01235","01235","01235","01235","01235","01235"],
+var users = { Name:["Line Vancauwenberghe","Giovanni Van Assche","David Debode","Stephane Lejour","Michaël Martens","Christophe Declercq",
+                    "Paul Herregods","Luc Buyens","Mircea Danila","Patrick Mulier","Ayoub Khamlich","Armand Bayart","Davy Vercruysse",
+                    "Thierry Verza"],             
+    Psw:["05193utb","06044utb","04970utb","04439utb","04549utb","04922utb",
+         "02197utb","03464utb","05978utb","02200utb","11037utb","05840utb","05838utb",
+         "05615utb"],
     Taalcode:["NL","NL","FR","FR","NL","FR","NL","NL","FR","FR","FR","FR","NL","FR"]}; 
 
 
@@ -16,7 +20,15 @@ function check(form){
        valid = true;
        
           break;  
-      }     
+      } 
+      else{
+          if (users.Taalcode == "NL"){
+             alert("Combinatie van gebruikersnaam en wachtwoord komt niet overeen");
+            }
+          if if (users.Taalcode == "FR"){
+             alert("Combinaison du nom d'utilisateur et du mot de passe ne correspond pas");
+          }
+      }
    }   
    
    if (valid)   {    
